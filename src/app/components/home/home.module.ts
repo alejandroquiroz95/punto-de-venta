db65165cb885728b452e441ed0c86b04d5bbc0d3
Alejandro <quirozalejandro923@gmail.com>
2024-06-12 13:16:56 -0600

    commit inicial
    - Integre Bootstrap
    - Agregue el enrutamiento.
    - Agregue pantalla de login.
    - Agregue pantalla de home

---
+++ src/app/components/home/home.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+//custom
+import { HomeComponent } from "./home.component";
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+  ],
+})
+export class HomeModule { }
